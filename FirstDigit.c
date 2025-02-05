83bb8c94fae82592d1e04f44389769d52ce2ec1b
arulkumarank <arulkumaran187.com>
2025-02-05 19:37:10 +0530

    FirstDigit

--- FirstDigit.c
+++ FirstDigit.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (){
     int a,b;
-    printf("enter a wo digit number : ");
+    printf("enter a two digit number : ");
     scanf("%d",&a);
     a=a%10;
     printf("the once place is : %d",a);
