a0590036cea02890f5ca2d467e6e258c9535b6bc
arulkumarank <arulkumaran187.com>
2025-02-05 19:10:00 +0530

    SubBy5

---
+++ SubBy5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    int a,b;
+    printf("enter the first number : ");
+    scanf("%d",&a);
+    b   = a-5, &b;
+    printf("the addiion of these numbers are : %d",b);
+    return 0;
+}
