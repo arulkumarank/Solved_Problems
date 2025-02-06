81548b9756fb17ce4afb9520e1f3e2a49a6cd9dd
arulkumarank <arulkumaran187.com>
2025-02-06 14:18:08 +0530

    TenPlace3Digit

---
+++ TenPlace3Digit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+    int a,b;
+    printf("enter a three digit number : ");
+    scanf("%d",&a);
+    a=a%100;
+    a=a/10;
+    printf("the once place is : %d",a);
+    return 0;
+}
